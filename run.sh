9854c221b7b3ae39c67ac56721950e354cb6f65c
Marinos Koutsomichalis <me@marinoskoutsomichalis.com>
2017-02-26 13:13:26 +0200

    made the built-audio the default output

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ PATH=$PATH:/usr/local/bin
 cd /home/chip/Inhibition
 now=$(date +"%m_%d_%Y")
 date >> "log_$now.log"
-#jackd -P75 -dalsa -Phw:0 -p8192 -n3 -s -r44100 & #if you use the default audio output
-jackd -P75 -dalsa -Phw:1 -p8192 -n3 -s -r44100 & #if you use the usb soundcard
+jackd -P75 -dalsa -Phw:0 -p8192 -n3 -s -r44100 & #if you use the default audio output
+#jackd -P75 -dalsa -Phw:1 -p8192 -n3 -s -r44100 & #if you use the usb soundcard
 sleep 15
 sclang main.scd >> "log_$now.log"
 exit 0
